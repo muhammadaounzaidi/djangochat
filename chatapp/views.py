a825c71c9d4d72b5c40880da05a82af99166070d
Muhammad Aoun Zaidi <zaoun123@gmail.com>
2024-07-30 12:29:37 +0500

    Add Consumer & Routers

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import ChatRoom
 # Create your views here.
+def index(request):
+    chatrrooms = ChatRoom.objects.all()
+    return render(request,'chatapp/index.html', context={"chatrooms":chatrrooms})     
+
+def chatroom(request,slug):
+    chatroom=ChatRoom.objects.get(slug=slug)
+    return render(request,'chatapp/chatroom.html', context={"chatroom":chatroom})
--- core/settings.py
+++ core/settings.py
@@ -73,6 +73,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 ASGI_APPLICATION = "core.asgi.application"
 
 
+CHANNEL_LAYERS ={
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
